5266f06aa5155c68d35188da637e83d866b8c78a
Esteban Briceño Peroza <162641471+00000Esteban1111@users.noreply.github.com>
2024-06-12 10:08:11 +0000

    Mejorado con boostrap

---
+++ index.js
@@ -0,0 +1,18 @@
+const excuses = [
+    "My dog ate my homework.",
+    "I lost track of time.",
+    "I had a family emergency.",
+    "My car broke down.",
+    "I was feeling under the weather.",
+    "There was a power outage.",
+    "I had internet issues.",
+    "I was stuck in traffic.",
+    "I had a doctor's appointment.",
+    "I forgot about the deadline."
+];
+
+function generateExcuse() {
+    const randomIndex = Math.floor(Math.random() * excuses.length);
+    const excuse = excuses[randomIndex];
+    document.getElementById('excuse').innerText = excuse;
+}
