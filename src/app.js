5d2d60e475ba6847ea0c9c3f48671ee7d31acbed
Esteban Briceño Peroza <162641471+00000Esteban1111@users.noreply.github.com>
2024-06-12 09:51:26 +0000

    pretier

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const excuses = [
+  "My dog ate my homework.",
+  "I lost track of time.",
+  "I had a family emergency.",
+  "My car broke down.",
+  "I was feeling under the weather.",
+  "There was a power outage.",
+  "I had internet issues.",
+  "I was stuck in traffic.",
+  "I had a doctor's appointment.",
+  "I forgot about the deadline."
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateExcuse() {
+  const randomIndex = Math.floor(Math.random() * excuses.length);
+  const excuse = excuses[randomIndex];
+  document.getElementById('excuse').innerText = excuse;
+}
